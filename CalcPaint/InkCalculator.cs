ea02259bd4fc1c92c5cfaae9c768cb92f0a1b5c6
tecnobert <mrrabbitomg@gmail.com>
2020-05-11 17:30:40 -0300

    Initial commit

---
+++ CalcPaint/InkCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace InkCalculator
+{
+   class Calculator
+    {
+       private double wall_area;
+       private double ceiling_area;
+
+        public double CalculateWallArea(double height, double width, double depth)
+        {
+            wall_area = 2 * (width + depth) * height;
+            return wall_area;
+        }
+
+        public double CalculateCeilingArea(double width, double depth)
+        {
+            ceiling_area = width * depth;
+            return ceiling_area;
+        }
+        public double CalculateRequiredLiteracy()
+        {
+            return (wall_area + ceiling_area) / 10;
+        }
+    }
+}
+
+
+
+
---
+++ CalcPaint/Program.cs
@@ -0,0 +1,34 @@
+﻿using InkCalculator;
+using System;
+
+namespace CalcPaint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            const double HEIGHT = 2.9; 
+            Console.WriteLine("How wide is the room?");
+            double width = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("How deep is the room?");
+            double depth = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("The area of ​​the walls is:");
+            Console.WriteLine(calculator.CalculateWallArea(HEIGHT, width, depth));
+
+          
+            Console.WriteLine("The ceiling area is:");
+            Console.WriteLine(calculator.CalculateCeilingArea(width, depth));
+
+            Console.WriteLine("The required paint finish is:");
+            double ink_litring = calculator.CalculateRequiredLiteracy();
+            Console.WriteLine(ink_litring);
+
+            Console.WriteLine("Finished!");
+            Console.ReadKey();
+        }
+    }
+}
